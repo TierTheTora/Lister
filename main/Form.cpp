a497c1bd058eb5ffb929268803ad1ff4e304b77a
TierTheTora <160141870+TierTheTora@users.noreply.github.com>
2024-10-25 16:55:43 +0200

    Create Form.cpp

---
+++ main/Form.cpp
@@ -0,0 +1,11 @@
+#include "Form.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThreadAttribute]
+int main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	window::MyForm frm;
+	Application::Run(% frm);
+}
